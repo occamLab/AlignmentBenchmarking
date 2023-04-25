e3394e4a39d63d878bd9ff17fcf29df940f36776
Anmol Sandhu <anmolrattansandhu@gmail.com>
2023-04-24 20:23:09 -0400

    added ENU space conversions

--- particleFilter.py
+++ particleFilter.py
@@ -9,6 +9,17 @@ from pyproj import Transformer
 from scipy.stats import multivariate_normal
 from scipy.linalg import inv
 
+def enu_to_ecef_rotation_matrix(lat, lon):
+    sin_lat = np.sin(np.radians(lat))
+    cos_lat = np.cos(np.radians(lat))
+    sin_lon = np.sin(np.radians(lon))
+    cos_lon = np.cos(np.radians(lon))
+
+    R = np.array([[-sin_lon, -sin_lat * cos_lon, cos_lat * cos_lon],
+                  [cos_lon, -sin_lat * sin_lon, cos_lat * sin_lon],
+                  [0, cos_lat, sin_lat]])
+    return R
+
 def create_particles(geoSpatialTransforms, N):
     particles = [dict() for _ in range(N)]
     lat, lon, alt = geoSpatialTransforms[0][:3]
@@ -27,11 +38,30 @@ def latlonalt_to_ecef(lat, lon, alt):
     x, y, z = transformer.transform(lat, lon, alt)
     return x, y, z
 
+def ar_pose_to_ecef(ar_pose, lat, lon, alt):
+    ar_pose_matrix = np.array(ar_pose).reshape(4, 4)
+
+    # Convert LLA to ECEF
+    x, y, z = latlonalt_to_ecef(lat, lon, alt)
+
+    # Get the rotation matrix from ENU to ECEF
+    R = enu_to_ecef_rotation_matrix(lat, lon)
+
+    # Construct the ECEF transformation matrix
+    T_ecef = np.eye(4)
+    T_ecef[:3, :3] = R
+    T_ecef[:3, 3] = [x, y, z]
+
+    # Convert AR pose from ENU to ECEF
+    ar_pose_ecef = np.dot(T_ecef, ar_pose_matrix)
+
+    return ar_pose_ecef
+
 def update_ecef_uncertainties(lat, lon, alt, lat_uncertainty, lon_uncertainty, alt_uncertainty):
     a = 6378137.0  # Earth's semi-major axis (m)
     f = 1 / 298.257223563  # Earth's flattening
     e2 = 2 * f - f ** 2  # Earth's first eccentricity squared
-    #Not very good
+    # Not very good
     N_phi = a / np.sqrt(1 - e2 * np.sin(np.radians(lat)) ** 2)
     x_uncertainty = np.abs((N_phi + alt) * np.cos(np.radians(lat)) * np.radians(lon_uncertainty))
     y_uncertainty = np.abs((N_phi * (1 - e2) + alt) * np.sin(np.radians(lat)) * np.radians(lat_uncertainty))
@@ -41,15 +71,27 @@ def update_ecef_uncertainties(lat, lon, alt, lat_uncertainty, lon_uncertainty, a
 def predict(particles, prev_lat, prev_lon, prev_alt, current_lat, current_lon, current_alt, prev_pose, pose, noise_std=1):
     prev_x, prev_y, prev_z = latlonalt_to_ecef(prev_lat, prev_lon, prev_alt)
     current_x, current_y, current_z = latlonalt_to_ecef(current_lat, current_lon, current_alt)
-    # dist_ecef = np.array([current_x - prev_x, current_y - prev_y, current_z - prev_z])
-    pose_difference = inv(pose) * prev_pose
-    noise = np.random.normal(0, noise_std, size=(len(particles), 3))
-    for i, particle in enumerate(particles):
-        particle['x'] += dist_ecef[0] + noise[i, 0]
-        particle['y'] += dist_ecef[1] + noise[i, 1]
-        particle['z'] += dist_ecef[2] + noise[i, 2]
+    dist_ecef = np.array([current_x - prev_x, current_y - prev_y, current_z - prev_z])
 
-def update(particles, weights, measurement_ecef, uncertainties, weight_effect=4000000):
+    pose = ar_pose_to_ecef(pose, current_lat, current_lon, current_alt)
+    prev_pose = ar_pose_to_ecef(prev_pose, prev_lat, prev_lon, prev_alt)
+
+    pose_difference = inv(pose) * prev_pose
+
+    translation = pose_difference[0:3, 3]
+    # rotation = pose_difference[0:3, 0:3]
+    noise = np.random.normal(0, noise_std, size=(len(particles), 3))
+    # for i, particle in enumerate(particles):
+    #     particle['x'] += dist_ecef[0] + noise[i, 0]
+    #     particle['y'] += dist_ecef[1] + noise[i, 1]
+    #     particle['z'] += dist_ecef[2] + noise[i, 2]
+    for i, particle in enumerate(particles):
+        particle['x'] += translation[0]
+        particle['y'] += translation[1]
+        particle['z'] += translation[2]
+        particle['pose'] = inv(pose_difference) @ particle['pose'] 
+
+def update(particles, weights, measurement_ecef, uncertainties, weight_effect=1):
     weights.fill(1.0)
 
     for i, p in enumerate(particles):
